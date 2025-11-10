ffab73505847d4ec013099444eed40c84644b989
Sriraj2911 <cs24btech11066@iith.ac.in>
2025-11-10 22:19:19 +0530

    Start Prim's logic

--- DSA/Coding Assignment 4/2.c
+++ DSA/Coding Assignment 4/2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "DataStructures.h"
 
 void addEdge(int n, Vector adj[n], int a, int b){
@@ -12,8 +13,31 @@ typedef struct{
     int w; // Weight of the edge
 }Edge;
 
-Edge* Prims(int n, Vector adj[n], int m, int arr[m]){
-    
+// bool to check if all the vertices were visited or not
+bool allVisited(int* visited, int n){
+    for(int i=0; i<n; i++){
+        if(visited[i] == 0) return false;
+    }
+    return true;
+}
+
+Edge* Prims(int n, Vector adj[n], int m, Edge edges[m]){
+    buildHeap(edges, m);
+    int visited[n];
+    for(int i=0; i<n; i++){
+        visited[i] = 0;
+    }
+    int src = edges[0].u; // Vertex of the edge with minimum weight
+    visited[src-1] = 1;
+
+    Edge MST[n-1]; // Array of edges in the MST
+
+    while(!allVisited(visited, n)){
+        int curr = src;
+        Vector temp = adj[curr-1]; // Vector with adjacent vertices of 'curr'
+        
+    }
+
 }
 
 int main(){
