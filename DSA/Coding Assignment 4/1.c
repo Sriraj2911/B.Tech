492dc68e859f87585d15f804ce1635c57dd4e9e4
Sriraj2911 <cs24btech11066@iith.ac.in>
2025-11-10 14:25:21 +0530

    Add the vertices along the path

--- DSA/Coding Assignment 4/1.c
+++ DSA/Coding Assignment 4/1.c
@@ -24,7 +24,6 @@ bool isFullQ(Queue *q, int n){
 
 void enqueue(Queue *q, int ele, int n){
     if(isFullQ(q, n)){
-        // printf("Queue overflow!\n");
         return;
     }
     q->data[q->rear] = ele;
@@ -34,7 +33,6 @@ void enqueue(Queue *q, int ele, int n){
 
 int dequeue(Queue *q, int n){
     if(isEmptyQ(q)){
-        // printf("Queue underflow!\n");
         return -1;
     }
     int val = q->data[q->front];
@@ -51,16 +49,13 @@ void printQ(Queue q, int n){
     printf("\n");
 }
 
-// bool inQ(Queue *q, int ele){
-
-// }
-
 void addEdge(int n, int adj[n][n], int a, int b){
     // a and b are assumed to be <=n
     adj[a-1][b-1] = 1;
     adj[b-1][a-1] = 1;
 }
 
+// A new struct for the vertices
 typedef struct{
     int value; 
     int length; // Distance from the source vertex
@@ -68,30 +63,43 @@ typedef struct{
 }Vertex;
 
 Vertex* bfs(int n, int adj[n][n], int src) {
-    int *visited = calloc(n, sizeof(int));
+    int *discovered = calloc(n, sizeof(int)); // An array to maintain the discovered vertices
     Queue *q = malloc(sizeof(Queue));
     initQ(q, n);
-
+    // Initialise the queue with the source vertex
     enqueue(q, src, n);
+    discovered[src-1] = 1;
+    // An array of Vertex data type
     Vertex *vertices = malloc(sizeof(Vertex)*n);
     for(int i=0; i<n; i++){
+        vertices[i].value = i+1;
+        vertices[i].length = -1;
         initQ(&(vertices[i].q), n);
     }
-
-    vertices[src-1].value = src;
+    // Initialize for the source vertex
     vertices[src-1].length = 0;
-    visited[src-1] = 1;
+    enqueue(&(vertices[src-1].q), src, n);
 
-    while (q->front < q->rear) {
-        int curr = dequeue(q, n);
-        enqueue(&(vertices[curr-1].q), curr, n);
+    while (!isEmptyQ(q)) {
+        int curr = dequeue(q, n); // Dequeuing the vertex means it has been visited
+
         for (int i = 0; i < n; i++) {
-            if (adj[curr-1][i] && !visited[i]) {
-                vertices[i].value = i + 1;
+            if (adj[curr-1][i] && !discovered[i]) {
+                discovered[i] = 1;
+
+                // Copy the path from curr to i
+                Queue temp = vertices[curr-1].q;
+                initQ(&(vertices[i].q), n);
+                for(int j=0; j<temp.count; j++){
+                    int idx = (temp.front+j)%n;
+                    enqueue(&(vertices[i].q), temp.data[idx], n);
+                }
+                // Add this vertex to the path
+                enqueue(&(vertices[i].q), i+1, n);
+                // Update the length of this vertex from the source
                 vertices[i].length = vertices[curr-1].length + 1;
-                enqueue(&(vertices[i].q), curr, n);
-                visited[i] = 1;
-                enqueue(q, i, n);
+                // Enqueue this vertex to the queue
+                enqueue(q, i+1, n);
             }
         }
     }
@@ -110,12 +118,14 @@ int main(){
             adj[i][j] = 0;
         }
     }
+    // Build the graph
     for(int i=0; i<m; i++){
         scanf("%d %d", arr, arr+1);
         addEdge(n, adj, arr[0], arr[1]);
     }
     int src;
     scanf("%d", &src);
+    
     Vertex *vertices = bfs(n, adj, src);
 
     for(int i=0; i<n; i++){
